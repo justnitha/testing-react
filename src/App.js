d91881f2cc1ba53aecabdc6b1f46d889b0ff86a1
Your Name <you@example.com>
2023-06-28 11:21:11 +0800

    dfsdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{ useState } from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {HashRouter as Router, Routes, Route} from "react-router-dom"
 import './App.css';
 
 import Notfound from "./Pages/Nav/Notfound";  
@@ -29,26 +29,3 @@ const App = function() {
 
 export default App;
 
- 
-//   const [getNavbarValue,setNavbarValue] = useState("");
-//   const [getNavbarValue2,setNavbarValue2] = useState("");
-  
-
-//   const changeNavbarValue= function() {
-//     alert("anda berhasil mengubah");
-//     setNavbarValue("myConctact" );
-//   };
-//   const changeNavbarValue2= function() {
-//     alert("anda berhasil mengubah");
-//     setNavbarValue2("menang");
-//   };
-//   return (
-//     <div className="container mx-auto">
-//       <Navigation navValue={getNavbarValue} nav2={getNavbarValue2}/>
-//       <h2 className="mt-10">INI Homepage</h2>
-//       {/* <button onClick={()=> changeNavbarValue(changeNavbarValue2())}>Ubah Navigasi</button> */}
-//       <button className="p-3 bg-purple-400  rounded-2xl" onClick={()=> changeNavbarValue()}>Ubah Navigasi</button>
-//       <button className="p-3 bg-orange-400  rounded-2xl" onClick={()=> changeNavbarValue2()}>ubah2</button>
-//       <p>lorem20</p>
-//     </div>
-//   );
