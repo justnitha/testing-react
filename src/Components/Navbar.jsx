9b31934bd4c1ba6b06f10dd8b6be420056c37f43
Your Name <you@example.com>
2023-06-28 11:42:34 +0800

    dfsdf

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
                 <img src={Logo} alt="logo" className="h-[6rem] mx-auto"/>
             </div>
             <ul className="flex gap-9 justify-center uppercase mt-7 text-sm">
-                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/about">About</a></li>
+                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/#/about">About</a></li>
                 <li class="dropdown z-10 bnn ">
-                    <a href="/testing-react" className="text-[#285538] font-bold">Brands <i class="fa-solid fa-chevron-down font-bold"></i></a>
+                    <a href="/testing-react/#/" className="text-[#285538] font-bold">Brands <i class="fa-solid fa-chevron-down font-bold"></i></a>
                     <div class="dropdown-content text-start">
                         <a href="#" class="btn active">Kedai Kopi Kulo</a>
                         <a href="#" class="btn">pochajjang</a>
@@ -27,10 +27,10 @@ const Navbar = () => {
                         <a href="#" class="btn">absolutea</a>
                     </div>
                 </li>
-                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/promo">promo</a></li>
-                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/app">app</a></li>
-                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/partnership">partnership</a></li>
-                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/contact">contact us</a></li>
+                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/#/promo">promo</a></li>
+                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/#/app">app</a></li>
+                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/#/partnership">partnership</a></li>
+                <li className="text-[#285538] font-bold bnn"><a href="/testing-react/#/contact">contact us</a></li>
                 
             </ul>
             
