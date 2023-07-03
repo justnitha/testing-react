6e82f06bb09a50ff11f44cfd1485b948dab86163
Your Name <you@example.com>
2023-07-03 18:16:43 +0800

    menambahkan image

--- src/Components/js/PromoGambar.js
+++ src/Components/js/PromoGambar.js
@@ -3,278 +3,279 @@ export const PromoGambar=[
         id:1,
         name:'Kulo1',
         category: 'Kulo',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/promo-kulo-agustus.jpg'
+        image : require('../../plugin/img/kulo1.jpg'),
     },
     {
         id:2,
         name:'Kulo2',
         category: 'Kulo',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/promo-kulo-agustus-4.jpg'
+        image : require('../../plugin/img/kulo2.jpg'),
 
     },
     {
         id:3,
         name:'Kulo3',
         category: 'Kulo',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/promo-kulo-agustus-3.jpg'
+        image : require('../../plugin/img/kulo3.jpg')
 
     },
     {
         id:4,
         name:'Kulo4',
         category: 'Kulo',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/promo-kulo-agustus-2.jpg'
+        image : require('../../plugin/img/kulo4.jpeg')
 
     },
     {
         id:5,
         name:'Pochijang1',
         category: 'Pochijang',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-pochajjang-agustus-3.jpg'
+        image : require('../../plugin/img/pochajjang2.jpg')
 
     },
     {
         id:6,
         name:'Pochijang2',
         category: 'Pochijang',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-pochajjang-agustus-4.jpg'
+        image : require('../../plugin/img/pochijang1.jpeg')
 
     },
     {
         id:7,
         name:'Pochijang3',
         category: 'Pochijang',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-pochajjang-agustus-5.jpg'
+        image : require('../../plugin/img/pochijang3.jpeg')
 
     },
     {
         id:8,
         name:'Pochijang4',
         category: 'Pochijang',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-pochajjang-agustus-.jpg'
+        image : require('../../plugin/img/pochijang4.jpeg')
 
     },
     {
         id:9,
         name:'Pochijang5',
         category: 'Pochijang',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-pochajjang-agustus-2.jpg'
+        image : require('../../plugin/img/pochijang5jpeg.jpeg')
 
     },
     {
         id:10,
         name:'Xiboba1',
         category: 'Xiboba',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Xiboba-Agustus-4.jpg'
+        image : require('../../plugin/img/xiboba1.jpeg')
 
     },
     {
         id:11,
         name:'Xiboba2',
         category: 'Xiboba',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Xiboba-Agustus-3.jpg'
+        image : require('../../plugin/img/xiboba2.jpeg')
 
     },
     {
         id:12,
         name:'Xiboba3',
         category: 'Xiboba',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Xiboba-Agustus-2.jpg'
+        image :  require('../../plugin/img/xiboba3.jpeg')
 
     },
     {
         id:13,
         name:'Xiboba4',
         category: 'Xiboba',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Xiboba-Agustus-1.jpg'
+        image :  require('../../plugin/img/xiboba4.jpeg')
 
     },
     {
         id:14,
         name:'Kitamura1',
         category: 'Kitamura',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Kitamura-Agustus-1.jpg'
+        image : require('../../plugin/img/kitamura1.jpeg')
 
     },
     {
         id:15,
         name:'Kitamura2',
         category: 'Kitamura',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Kitamura-Agustus-4.jpg'
+        image : require('../../plugin/img/kitamura2.jpeg')
 
     },
     {
         id:16,
         name:'Kitamura3',
         category: 'Kitamura',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Kitamura-Agustus-3.jpg'
+        image : require('../../plugin/img/kitamura3.jpeg')
 
     },
     {
         id:17,
         name:'Kitamura4',
         category: 'Kitamura',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Kitamura-Agustus-2.jpg'
+        image : require('../../plugin/img/kitamura4.jpeg')
 
     },
     {
         id:18,
         name:'Bu-Eva1',
         category: 'Bu-eva',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-bu-eva-agustus3.jpg'
+        image : require('../../plugin/img/bu-eva1.jpeg')
+
 
     },
     {
         id:19,
         name:'Bu-eva2',
         category: 'Bu-eva',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-bu-eva-agustus2.jpg'
+        image : require('../../plugin/img/bu-eva2.jpeg')
 
     },
     {
         id:20,
         name:'Bu-eva3',
         category: 'Bu-eva',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-bu-eva-agustus.jpg'
+        image : require('../../plugin/img/bu-eva3.jpg')
 
     },
     {
         id:21,
         name:'Mazeru1',
         category: 'Mazeru',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/mazeru2.jpg'
+        image : require('../../plugin/img/mazeru2.jpeg')
     },
     {
         id:22,
         name:'Mazeru2',
         category: 'Mazeru',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/mazeru3.jpg'
+        image : require('../../plugin/img/mazeru.jpeg')
     },
     {
         id:23,
         name:'Mazeru3',
         category: 'Mazeru',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/mazeru1.jpg'
+        image : require('../../plugin/img/mazeru1.jpeg')
     },
     {
         id:24,
         name:'Mazeru4',
         category: 'Mazeru',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/mazeru4.jpg'
+        image : require('../../plugin/img/mazeru3.jpeg')
     },
     {
         id:25,
         name:'Oseng-mie1',
         category: 'Oseng-mie',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/omj6.jpg'
+        image : require('../../plugin/img/osengmie1.jpeg')
     },
     {
         id:26,
         name:'Oseng-mie2',
         category: 'Oseng-mie',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/omj5.jpg'
+        image : require('../../plugin/img/oseng-mie2.jpeg')
     },
     {
         id:27,
         name:'Oseng-mie3',
         category: 'Oseng-mie',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/omj2.jpg'
+        image : require('../../plugin/img/oseng-mie3.jpeg')
     },
     {
         id:28,
         name:'Oseng-mie4',
         category: 'Oseng-mie',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/omj1.jpg'
+        image : require('../../plugin/img/oseng-mie4.jpeg')
     },
     {
         id:29,
         name:'Oseng-mie5',
         category: 'Oseng-mie',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/omj3.jpg'
+        image : require('../../plugin/img/oseng-mie5.jpeg')
     },
     {
         id:30,
         name:'Oseng-mie6',
         category: 'Oseng-mie',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/omj4.jpg'
+        image : require('../../plugin/img/oseng-mie6.jpeg')
     },
     {
         id:31,
         name:'Xiji1',
         category: 'Xiji',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Xiji-Agustus-3.jpg'
+        image :require('../../plugin/img/xiji-1.jpeg') 
     },
     {
         id:32,
         name:'Xiji2',
         category: 'Xiji',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Xiji-Agustus-2.jpg'
+        image : require('../../plugin/img/xiji-2.jpeg')
     },
     {
         id:33,
         name:'Xiji3',
         category: 'Xiji',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Xiji-Agustus.jpg'
+        image : require('../../plugin/img/xiji-3.jpeg')
     },
     {
         id:34,
         name:'Mo-tahu-aja1',
         category: 'Mo-tahu-aja',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Mo-Tahu-Aja-Agustus-2-1.jpg'
+        image : require('../../plugin/img/mo-tahu1.jpeg')
     },
     {
         id:35,
         name:'Mo-tahu-aja2',
         category: 'Mo-tahu-aja',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Mo-Tahu-Aja-Agustus-4.jpg'
+        image : require('../../plugin/img/mo-tahu2.jpeg')
     },
     {
         id:36,
         name:'Mo-tahu-aja2',
         category: 'Mo-tahu-aja',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Mo-Tahu-Aja-Agustus-3.jpg'
+        image : require('../../plugin/img/mo-tahu3.jpeg')
     },
     {
         id:37,
         name:'Mo-tahu-aja3',
         category: 'Mo-tahu-aja',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Mo-Tahu-Aja-Agustus-1.jpg'
+        image : require('../../plugin/img/mo-tahu4.jpeg')
     },
     {
         id:38,
         name:'absolute1',
         category: 'absolute',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Absolutea-Agustus-3.jpg'
+        image : require('../../plugin/img/absolutea1.jpeg')
     },
     {
         id:39,
         name:'absolute2',
         category: 'absolute',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/Promo-Absolutea-Agustus-2.jpg'
+        image : require('../../plugin/img/absolutea2.jpeg')
     },
     {
         id:40,
         name:'ela-geprek',
         category: 'ela-geprek',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/promo-ela-agustus-.jpg'
+        image : require('../../plugin/img/elageprek2.jpeg')
     },
     {
         id:41,
         name:'ela-geprek1',
         category: 'ela-geprek',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/promo-ela-agustus-2.jpg'
+        image : require('../../plugin/img/elageprek1.jpeg')
     },
     {
         id:42,
         name:'ela-geprek2',
         category: 'ela-geprek',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/promo-ela-agustus-3.jpg'
+        image : require('../../plugin/img/elageprek3.jpeg')
     },
     {
         id:43,
         name:'ela-geprek3',
         category: 'ela-geprek',
-        image : 'https://kulogroup.com/wp-content/uploads/2021/08/promo-ela-agustus-4.jpg'
+        image : require('../../plugin/img/elageprek4.jpeg')
     },
     
 ]
